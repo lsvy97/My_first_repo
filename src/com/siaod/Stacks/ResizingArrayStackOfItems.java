4c27d749c6b568a3f3ecd06f9833f26e7c352f10
lsvy97 <diamondfinder7@gmail.com>
2017-11-08 11:13:30 +0700

    Added ResizingArrayStackOfItems.java

--- src/com/siaod/Stacks/ResizingArrayStackOfItems.java
+++ src/com/siaod/Stacks/ResizingArrayStackOfItems.java
@@ -1,23 +1,25 @@
 package com.siaod.Stacks;
 
-public class ResizingArrayStackOfStrings {
-    private String[] s;
+public class ResizingArrayStackOfItems<Item> {
+    private Item[] s;
     private int N = 0;
 
-    public ResizingArrayStackOfStrings() {s = new String[1];}
+    public ResizingArrayStackOfItems() {
+        s = (Item[]) new Object[1];
+    }
 
-    public void push(String item) {
+    public void push(Item item) {
         if (N == s.length) resize(2 * s.length);
         s[N++] = item;
     }
-    public String pop() {
-        String item = s[--N];
+    public Item pop() {
+        Item item = s[--N];
         s[N] = null;
         if (N > 0 && N == s.length/4) resize(s.length/2);
         return item;
     }
-    private void resize(int capacity){
-        String[] copy = new String [capacity];
+    private void resize(int capacity) {
+        Item[] copy = (Item[]) new Object[capacity];
         for (int i = 0; i < N; i++) {
             copy[i] = s[i];
         }
